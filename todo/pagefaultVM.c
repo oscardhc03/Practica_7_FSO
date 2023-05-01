27ee1e2416a80cd8afd7ec31ca8880869a90bfa9
oscardhc03 <60898622+oscardhc03@users.noreply.github.com>
2023-04-30 21:01:50 -0600

    new
    gg

--- todo/pagefaultVM.c
+++ todo/pagefaultVM.c
@@ -30,62 +30,84 @@ int getfifo();
 int pagefault(char *vaddress)
 {
     int i;
-    int frame,vframe; 
+    int frame, vframe = -1; // vframe inicializado a -1
     long pag_a_expulsar;
-    int fd;
     char buffer[PAGESIZE];
     int pag_del_proceso;
 
-    // A partir de la dirección que provocó el fallo, calculamos la página
-    pag_del_proceso=(long) vaddress>>12;
+    // Calculamos la página a partir de la dirección que provocó el fallo
+    pag_del_proceso = (long) vaddress >> 12;
 
-
-    // Si la página del proceso está en un marco virtual del disco
+    // Buscamos si la página está en un marco virtual del disco
+    for (i = 0; i < ptlr->numpages; i++)
     {
-
-		// Lee el marco virtual al buffer
-
-        // Libera el frame virtual
-    }
-
-
-    // Cuenta los marcos asignados al proceso
-    i=countframesassigned();
-
-    // Si ya ocupó todos sus marcos, expulsa una página
-    if(i>=RESIDENTSETSIZE)
-    {
-		// Buscar una página a expulsar
-		
-		// Poner el bitde presente en 0 en la tabla de páginas
-        
-        // Si la página ya fue modificada, grábala en disco
+        // Si encontramos la página en un marco virtual
+        if (ptlr->ptentry[i].virtualframe == pag_del_proceso)
         {
-			// Escribe el frame de la página en el archivo de respaldo y pon en 0 el bit de modificado
+            // Leemos el marco virtual al buffer
+            vframe = ptlr->ptentry[i].virtualframe;
+            readblock(buffer, vframe);
+            
+            // Marcamos la página como no presente y liberamos el marco virtual
+            ptlr->ptentry[i].presente = 0;
+            ptlr->ptentry[i].virtualframe = -1;
+            break;
         }
-		
-        // Busca un frame virtual en memoria secundaria
-		// Si no hay frames virtuales en memoria secundaria regresa error
-		{
-            return(-1);
+    }
+
+    // Contamos los marcos asignados al proceso
+    i = countframesassigned();
+
+    // Si el proceso ya ocupó todos sus marcos
+    if(i >= RESIDENTSETSIZE)
+    {
+        // Buscamos una página para expulsar usando la política FIFO
+        pag_a_expulsar = getfifo();
+
+        // Marcamos la página como no presente
+        ptlr->ptentry[pag_a_expulsar].presente = 0;
+
+        // Si la página fue modificada, la guardamos en disco
+        if (ptlr->ptentry[pag_a_expulsar].modificado)
+        {
+            saveframe(ptlr->ptentry[pag_a_expulsar].frame);
+            ptlr->ptentry[pag_a_expulsar].modificado = 0;
         }
-        // Copia el frame a memoria secundaria, actualiza la tabla de páginas y libera el marco de la memoria principal
+
+        // Buscamos un marco virtual en memoria secundaria
+        vframe = searchvirtualframe();
+        
+        // Si no hay marcos virtuales en memoria secundaria, regresamos error
+        if(vframe == -1)
+        {
+            return -1;
+        }
+
+        // Copiamos el marco a memoria secundaria, actualizamos la tabla de páginas y liberamos el marco de la memoria principal
+        copyframe(ptlr->ptentry[pag_a_expulsar].frame, vframe);
+        ptlr->ptentry[pag_a_expulsar].virtualframe = vframe;
+        freeframe(ptlr->ptentry[pag_a_expulsar].frame);
     }
 
-    // Busca un marco físico libre en el sistema
-	// Si no hay marcos físicos libres en el sistema regresa error
+    // Buscamos un marco físico libre en el sistema
+    frame = getfreeframe();
+    
+    // Si no hay marcos físicos libres en el sistema, regresamos error
+    if(frame == -1)
     {
-        return(-1); // Regresar indicando error de memoria insuficiente
+        return -1; // Regresar indicando error de memoria insuficiente
     }
 
-    // Si la página estaba en memoria secundaria
+    // Si la página estaba en memoria secundaria (vframe != -1)
+    if (vframe != -1)
     {
-        // Cópialo al frame libre encontrado en memoria principal y transfiérelo a la memoria física
+        // Copiamos el contenido del buffer (marco virtual) al marco físico libre encontrado
+        memcpy(systemframetable[frame].paddress, buffer, PAGESIZE);
     }
-   
-	// Poner el bit de presente en 1 en la tabla de páginas y el frame 
 
+    // Poner el bit de presente en 1 en la tabla de páginas y asignar el marco físico
+    ptlr->ptentry[pag_del_proceso].presente = 1;
+    ptlr->ptentry[pag_del_proceso].frame = frame;
 
-    return(1); // Regresar todo bien
+    return 1; // Regresar todo bien
 }
-
